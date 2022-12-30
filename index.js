0b03e8926a310c0ddad5282d6e6ed16e58c51fef
= <=>
2022-12-30 11:30:18 +0500

    some comments code removed

--- index.js
+++ index.js
@@ -16,21 +16,6 @@ app.use("/testing", (req, res) => {
   res.send("testing");
 });
 app.use("/", routes);
-// cloudinary.config({
-//   cloud_name: "dr93dv5qy",
-//   api_key: "836564686949554",
-//   api_secret: "MhP8xdjFcCU4K4NtGfeOBGNMTp0",
-// });
-
-// cloudinary.uploader
-//   .upload("sample.pdf")
-//   .then((result) => console.log(`result is `, result))
-//   .catch((err) => {
-//     for (var i in err) {
-//       console.log("error is ", err[i]);
-//     }
-//     // console.log(`error is ${err}`);
-//   });
 
 app.listen(3002, (err) => {
   if (err) console.log(`errorr is ${err}`);
